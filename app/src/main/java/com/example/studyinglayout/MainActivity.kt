c6e2e00dc78efab6cc6c4e18c266f9499e0edd12
Михаил Бадерко <badmeha05@gmail.com>
2026-02-26 23:39:41 +0500

    Lesson 10: ViewBinding

--- app/src/main/java/com/example/studyinglayout/MainActivity.kt
+++ app/src/main/java/com/example/studyinglayout/MainActivity.kt
@@ -2,18 +2,27 @@ package com.example.studyinglayout
 
 import android.graphics.Color
 import android.os.Bundle
-import android.widget.TextView
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.ViewCompat
-import androidx.core.view.WindowInsetsCompat
+import com.example.studyinglayout.databinding.ActivityLearnWordBinding
 
 class MainActivity : AppCompatActivity() {
+    private var _binding: ActivityLearnWordBinding? = null
+    private val binding
+        get() = _binding ?: throw IllegalStateException("Binding for ActivityLearnWordBinding must not be null")
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
         enableEdgeToEdge()
+
+        _binding = ActivityLearnWordBinding.inflate(layoutInflater)
+
+        setContentView(binding.root)
+
         setContentView(R.layout.activity_learn_word)
 
+        /*
         val tvQuestionWord: TextView = findViewById<TextView>(R.id.tvQuestionWord)
 
         //Переопределение свойства text объекта tvQuestionWord
@@ -24,8 +33,15 @@ class MainActivity : AppCompatActivity() {
 
         //Вызов метода сеттера изменения цвета, свой цвет в формате HEX
         tvQuestionWord.setTextColor(Color.parseColor("#FDD600"))
+        */
 
+        binding.tvFirstAnswer.text = "Android"
+        binding.tvFirstAnswer.setTextColor(Color.GRAY)
 
-
+        with(binding){
+            tvSecondAnswer.setTextColor(Color.CYAN)
+            tvThirdAnswer.setTextColor(Color.GREEN)
+            tvFourthAnswer.setTextColor(Color.MAGENTA)
+        }
     }
 }
